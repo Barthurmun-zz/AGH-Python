0573f0e047ff445594c2a689e1dfa89a833eb7b9
Jakub <jakub@debian.debian>
2018-10-28 16:43:44 +0100

    Aktualizacje skryptów

---
+++ lab2/zad2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class LiczbyZespolone(object):
+    '''Ta klasa przechowuje i pozwala na operacje na liczbach zespolonych'''
+    def __init__(self,re,im):
+        self.im = im
+        self.re = re
+    
+    
+    
